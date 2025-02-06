09bd450dccdfa2fe29447a34ca6dab8913e2c32a
Rogerku <rogerwanb@gmail.com>
2025-02-06 21:34:59 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -172,7 +172,7 @@ const Index = () => {
                 loop 
                 playsInline
               >
-                <source src="guidevideo" type="video/mp4" />
+                <source src="/guidevideo" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </motion.div>
